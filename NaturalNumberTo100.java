c065309796224f64bf0382e2588055759db02838
Kiran Prajapati <kiranprajapati2719@gmail.com>
2017-04-28 13:13:28 +0545

    Add files via upload

---
+++ NaturalNumberTo100.java
@@ -0,0 +1,20 @@
+package Test;
+
+/**
+ * Created by Com 7 on 4/28/2017.
+ */
+public class NaturalNumberTo100 {
+
+    public static void main (String[] args){
+
+        int i;
+
+        for(i=1; i<=100; i++)
+        {
+            System.out.println( +i );
+
+        }
+
+    }
+
+}
